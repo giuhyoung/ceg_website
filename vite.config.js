07e9f6346dbb6fec0e76ce2e850c2405bd0067ce
giuhyoung <giuhyoung@gmail.com>
2026-05-04 22:29:15 +0900

    구식브라우저 검은화면 문제 해결(module 추가)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    legacy({
+      // Provide a broad baseline for older mobile browsers / in-app webviews.
+      targets: ['defaults', 'not IE 11'],
+    }),
+    react(),
+    tailwindcss(),
+  ],
 })
